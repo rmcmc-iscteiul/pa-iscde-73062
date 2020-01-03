dbb7ae5147a1dd54ea85bfc2c37af39666d71fb0
rmcmc-iscteiul <rmcmc@iscte-iul.pt>
2020-01-03 18:27:53 +0000

    bug fixes

--- pt.iscte.pidesco.outline/src/pt/iscte/pidesco/outline/internal/OutlineservicesImpl.java
+++ pt.iscte.pidesco.outline/src/pt/iscte/pidesco/outline/internal/OutlineservicesImpl.java
@@ -14,40 +14,43 @@ import pt.iscte.pidesco.outline.service.OutlineServices;
 import java.util.List;
 
 public class OutlineservicesImpl implements OutlineServices{
-	
+
 
 	@Override
 	public void addListener(OutListener listener) {
 		OutlineActivator.getInstance().addListener(listener);
-		
+
 	}
-	
+
 	@Override
 	public void removeListener(OutListener listener) {
 		OutlineActivator.getInstance().removeListener(listener);
-		
+
 	}
 
 
 	@Override
 	public void highlightText(String methodname) {
 
-		TreeViewer t = OutlineView.getInstance().getTree();
-		Tree tree = t.getTree();
-		
-		for (TreeItem ti : tree.getItems()) {
-			if (ti.getText().contains(methodname)) {
-				ti.setBackground(new Color(null, 0,0,255));
+		if(!(methodname == null)) {
+			TreeViewer t = OutlineView.getInstance().getTree();
+			Tree tree = t.getTree();
+
+			for (TreeItem ti : tree.getItems()) {
+				if (ti.getText().contains(methodname)) {
+					ti.setBackground(new Color(null, 0,0,255));
+				}
 			}
 		}
-		
-		
+
+
+
 	}
-	
-	
-	
 
 
 
-	
+
+
+
+
 }
